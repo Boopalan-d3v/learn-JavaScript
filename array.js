36d96b8b396d7136ed5f1f901df1df737fb10d42
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-06 19:58:34 +0530

    learned about array

---
+++ array.js
@@ -0,0 +1,10 @@
+//Array and functions are objects
+
+let courses= ['hacking','mern stack','spring boot', 7, true, null]
+console.log(courses[2])
+console.log(courses[4])
+console.log(courses[5])
+
+
+console.log(typeof(courses)) //object
+console.log(courses)
