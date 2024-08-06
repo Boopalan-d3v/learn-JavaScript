3efcd1112ff6d75fa9a57f080eda02a81e59f098
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-06 19:03:03 +0530

    learn about object in reference types

---
+++ Reference-types.js
@@ -0,0 +1,13 @@
+//Reference Types - Objects, Arrays, Functions
+
+let course = {
+    title:"cyber security",
+    descreption:"Basics of cyber security",
+    rating:5
+}
+console.log(course)
+console.log(typeof(course))
+
+console.log(course.title) //it's called dot notation
+console.log(course['rating']) //it's called bracket notation
+
