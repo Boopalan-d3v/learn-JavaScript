c40883d55e8f66f4119cdb53795aa7007246ed00
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-06 18:54:18 +0530

    changing value of pre-declared variable

--- javascrip.js
+++ javascrip.js
@@ -3,15 +3,17 @@ console.log("HEllo World");
 //Primitive /value types
 
 //string
+//JS is Dynamic language
+
 
 let x = "savage"
 console.log(x)
 console.log(typeof(x))
 
 //Null
- let v = null
- console.log(v)
- console.log(typeof(v))
+ x= null //here i changed value of predeclared variable
+ console.log(x)
+ console.log(typeof(x))
 
 //number
 
@@ -30,3 +32,5 @@ console.log(typeof(z))
 let p = true
 console.log(p)
 console.log(typeof(p))
+
+//string, null, number, Boolean
