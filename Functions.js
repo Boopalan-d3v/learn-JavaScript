986650545b32ecaf502025f9a81c9e166534925c
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-06 20:12:04 +0530

    learn that we can call function earlier

--- Functions.js
+++ Functions.js
@@ -1,3 +1,5 @@
+createCourse('mongoDB') //it can be accessible by the function 
+
 function createCourse(course){
     console.log('creating ' + course + ' course')
 }
@@ -5,4 +7,4 @@ function createCourse(course){
 createCourse('hacking')
 createCourse('react')
 createCourse('node')
-createCourse('mongoDB')
+
