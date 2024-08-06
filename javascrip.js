c65133ed8c1de79da67b362bb4986994d41af38d
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-06 18:42:28 +0530

    Data types of the values

---
+++ javascrip.js
@@ -0,0 +1,27 @@
+console.log("HEllo World");
+
+//Primitive /value types
+
+//string
+
+let x = "savage"
+console.log(x)
+console.log(typeof(x))
+
+//number
+
+
+let y = 1
+console.log(y)
+console.log(typeof(y))
+
+
+let z = 1.2
+console.log(z)
+console.log(typeof(z))
+
+//Boolean
+
+let p = true
+console.log(p)
+console.log(typeof(p))
