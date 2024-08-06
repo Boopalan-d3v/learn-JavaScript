b8a782fa501d674af424d7baba623604a56ec07e
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-06 23:56:44 +0530

    learn about variable types

---
+++ variable.js
@@ -0,0 +1,17 @@
+// Const, var, let
+// let and const where strict then var
+
+//const, let - Block scoped
+//var - Funtion scoped
+
+//lexical scope
+
+let a = 10;
+var b =20;
+const c = 30;
+
+function hello(){
+    let x = 20
+    console.log(x)
+}
+hello()
