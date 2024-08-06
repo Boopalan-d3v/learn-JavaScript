11c91ee8da908a85ccfd4611e7cb7d5ab3319b77
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-06 19:53:11 +0530

    Learned about value type and reference type

---
+++ value-reference.js
@@ -0,0 +1,15 @@
+let x = "savage"
+let y = x //value type
+x = "lorel"
+
+console.log(y) // savage
+console.log(x) // lorel
+
+let p = {
+    name:"Thunder"
+}
+let q = p //reference type
+p.name = "BLN"
+
+console.log(q) //BLN
+console.log(p) //BLN
