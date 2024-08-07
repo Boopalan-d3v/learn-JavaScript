7d8e20dcf16b2b2301119753f7f82354f94a6636
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-07 20:18:03 +0530

    Add string template in function

--- String-Template.js
+++ String-Template.js
@@ -1,8 +1,17 @@
 //String Template
-//`$(name)` it's for variable
-//`$(functionName())` it's for function
+//`${name}` it's for variable
+//`${functionName()}` it's for function
 
 let name = "Lorel"
 console.log('Hello, ' + name)
 
 console.log(`Hello, ${name}`) // Here, the both lines are returning the same output.
+
+let fullName = ( firstName, lastName) => {
+    return `${firstName} ${lastName}`
+}
+
+let result = fullName('Tony','Stark')
+
+console.log(result)
+
