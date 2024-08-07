da45a4abdb5a9bbc06fad585b5f4a0dacb7a4bb7
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-07 19:41:04 +0530

    Learn about Template string

---
+++ String-Template.js
@@ -0,0 +1,8 @@
+//String Template
+//`$(name)` it's for variable
+//`$(functionName())` it's for function
+
+let name = "Lorel"
+console.log('Hello, ' + name)
+
+console.log(`Hello, ${name}`) // Here, the both lines are returning the same output.
