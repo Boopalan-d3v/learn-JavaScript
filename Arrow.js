c11f0ede1aef77ced1eb47b93a3e063ce29b8184
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-07 20:36:14 +0530

    Learn about Arrow function

---
+++ Arrow.js
@@ -0,0 +1,12 @@
+//Arrow function
+
+function nickName(name){
+    return name;
+}
+
+console.log(nickName('Ashi singh'))
+
+let nName = name => `My name is ${name}` //Arrow function 
+
+console.log(nName('Ashi'))
+
